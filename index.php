2b2a5f5b63d50794b55c61eb00544d157928e614
Luis Paulo Lessa de Assis Junior <luis.junior@senacrjedu.local>
2018-11-27 10:21:47 -0200

    Webservice com saída padrão JSON

--- index.php
+++ index.php
@@ -26,4 +26,14 @@
     )
   );
 
-  print_r($cardio_dia);
+  $arrayConvertidoEmJson = json_encode($cardio_dia);
+  echo $arrayConvertidoEmJson;
+
+
+  /*
+  # Converter JSON para Array
+  $array = '[{"titulo":"Batatas r\u00fasticas super saborosas","categoria":"Entrada","imagem":"https:\/\/pt.petitchef.com\/imgupl\/recipe\/batatas-rusticas-super-saborosas--md-341832p554029.jpg","descricao":"Um r\u00fastico acompanhamento, arom\u00e1tico e saboroso para acompanhar carnes, sucesso garantido em sua refei\u00e7\u00e3o."},{"titulo":"Caril de frango f\u00e1cil","categoria":"Principal","imagem":"https:\/\/pt.petitchef.com\/imgupl\/recipe\/caril-de-frango-facil--md-449716p695097.jpg","descricao":"Caril de Frango ou Frango de Caril, tradicional da culin\u00e1ria de Goa, Dam\u00e3o e Diu, antes pertencentes ao Estado Portugu\u00eas da \u00cdndia, o Caril de Frango \u00e9 mais conhecido como Frango de Caril em Portugal e Mo\u00e7ambique tamb\u00e9m. Veja a receita PetitChef desta especialidade Indo-Portuguesa."},{"titulo":"Doce de chocolate","categoria":"Sobremesa","imagem":"https:\/\/pt.petitchef.com\/imgupl\/recipe\/sobremesa-facil-de-chocolate--md-47331p58059.jpg","descricao":"Com uma apar\u00eancia muito bonita e apetitosa! Servida com um sorvete de creme fica maravilhosa!"}]';
+
+  print_r(json_decode($array));
+
+  */
